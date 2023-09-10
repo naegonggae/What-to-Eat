c420df596e1cf4a8c59e08ed247621292e326178
naegonggae <tkdtkd97@naver.com>
2023-09-10 19:32:15 +0900

    :sparkles: test

---
+++ src/test/java/com/home/whattoeat/ControllerTest.java
@@ -0,0 +1,18 @@
+package com.home.whattoeat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerTest {
+
+	@Test
+	public void test() throws Exception {
+		Assertions.assertEquals(1, 1);
+	}
+
+
+}
