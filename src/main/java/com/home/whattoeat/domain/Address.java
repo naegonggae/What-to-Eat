77fe69c1beceae59610a6e7b5bcddc223befcdb4
naegonggae <tkdtkd97@naver.com>
2023-10-05 00:15:30 +0900

    :hammer: 코드 정리

--- src/main/java/com/home/whattoeat/domain/Address.java
+++ src/main/java/com/home/whattoeat/domain/Address.java
@@ -1,19 +1,22 @@
 package com.home.whattoeat.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
 	private String city;
 	private String street;
 	private String zipcode;
 
-	protected Address() {
+	public static Address createAddress(String city, String street, String zipcode) {
+		return new Address(city, street, zipcode);
 	}
-
 }
